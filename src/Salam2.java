534e1fc0560c20e6ff1f16148a7e3fff94513597
Aman <Aman@LAPTOP-BDBCPBRD>
2023-01-15 17:48:52 -0500

    7th Commit  Commit!

--- src/Salam2.java
+++ src/Salam2.java
@@ -1,7 +1,8 @@
 public class Salam2 {
 
     public static void main(String[] args) {
-        System.out.println("this is my First Commit");
+        System.out.println("this is my First C';gklommit");
+        System.out.println("this is second rfile");
     }
 
 
