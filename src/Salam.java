67e6aa83f225a3db52aea52e0ce94371d07bcd86
Aman <Aman@LAPTOP-BDBCPBRD>
2023-01-15 17:06:28 -0500

    5th Commit  Commit!

--- src/Salam.java
+++ src/Salam.java
@@ -1,5 +1,5 @@
 public class Salam {
     public static void main(String[] args) {
-        System.out.println("whuytrereto");
+        System.out.println("whuytrkkjghkjgkjgkjhgjhgljhlkjghkljhereto");
     }
     }
