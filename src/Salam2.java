662d1cdea48522b5ab85428d1445ec7cdcf8d21a
Aman <Aman@LAPTOP-BDBCPBRD>
2023-01-15 18:01:32 -0500

    9th Commit  Commit!

--- src/Salam2.java
+++ src/Salam2.java
@@ -2,6 +2,7 @@ public class Salam2 {
 
     public static void main(String[] args) {
         System.out.println("12345,mb mn32");
+        System.out.println("it would be better to initiate it");
     }
 
 
